d8133b27edfbb0fc72a9ca8c4ce37849ee36b57d
Ranieri Althoff <ranisalt@gmail.com>
2016-05-11 16:00:03 -0300

    Add project scaffolding

---
+++ server.js
@@ -0,0 +1,85 @@
+#! /usr/bin/env node
+'use strict'
+
+/**
+ * Module dependencies.
+ */
+
+import http from 'http'
+import debug from 'debug'
+import app from './'
+
+/**
+ * Get port from environment.
+ */
+
+const port = do {
+  /**
+   * Normalize a port into a number, string, or false.
+   */
+
+  const port = parseInt(process.env.PORT || 3000, 10)
+
+  if (isNaN(port)) {
+      // named pipe
+    process.env.PORT
+  } else if (port >= 0) {
+    // port number
+    port
+  } else {
+    false
+  }
+}
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app.callback())
+
+  /**
+   * Event listener for HTTP server "error" event.
+   */
+
+const onError = error => {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+
+      // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES': {
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+      break
+    }
+    case 'EADDRINUSE': {
+      console.error(bind + ' is already in use')
+      process.exit(1)
+      break
+    }
+    default: {
+      throw error
+    }
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+const onListening = () => {
+  const addr = server.address()
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
+  debug('tickt:server')('Listening on ' + bind)
+}
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
