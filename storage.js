3894bf8be93cdb00c2670126889e15d5a13d9ebd
Ranieri Althoff <ranisalt@gmail.com>
2016-06-02 03:14:48 -0300

    Asyncify updater

--- storage.js
+++ storage.js
@@ -3,6 +3,7 @@
 import bluebird from 'bluebird'
 import redis from 'redis'
 
+bluebird.promisifyAll(redis.Multi.prototype)
 bluebird.promisifyAll(redis.RedisClient.prototype)
 const client = redis.createClient(process.env.REDIS_URL)
 
--- update.js
+++ update.js
@@ -2,28 +2,25 @@
 'use strict'
 
 import cheerio from 'cheerio'
-import request from 'request'
+import cloudinary from 'cloudinary'
+import request from 'request-promise'
 import client from './storage'
 
-const replMap = {
-  'bife': 'chinelo',
-  'quibe': 'quibe :(',
-}
-
-const regex = new RegExp(Object.keys(replMap).join('|'))
-const ruify = words => words.replace(regex, match => replMap[match])
-const sanitize = words => words.toLowerCase().replace('/', ' e ').trim()
-
-request({
-  gzip: true,
-  uri: 'http://ru.ufsc.br/ru/',
-}, (err, res, html) => {
-  if (err) {
-    throw err
+(async () => {
+  const replMap = {
+    'bife': 'chinelo',
+    'quibe': 'quibe :(',
   }
 
-  const $ = cheerio.load(html)
+  const regex = new RegExp(Object.keys(replMap).join('|'))
+  const ruify = words => words.replace(regex, match => replMap[match])
+  const sanitize = words => words.toLowerCase().replace('/', ' e ').trim()
 
+  const $ = await request({
+    gzip: true,
+    transform: cheerio.load,
+    uri: 'http://ru.ufsc.br/ru/',
+  })
   // adjust date to UTC-3, make sunday wrap around and offset header
   const weekday = (new Date().getDay() - 1) % 7 + 2
   const cells = $(`.entry > table:first-of-type tr:nth-child(${weekday}) td:nth-child(n+4)`)
@@ -32,16 +29,12 @@ request({
     throw new Error('Failure to parse HTML')
   }
 
-  client.multi()
+  await client.multi()
     .set('lunch:main', ruify(sanitize(cells.eq(0).text())))
     .set('lunch:complement', sanitize(cells.eq(1).text()))
     .set('lunch:salad', sanitize(cells.eq(2).text()))
     .set('lunch:dessert', sanitize(cells.eq(3).text()))
-    .exec(err => {
-      if (err) {
-        throw err
-      }
+    .execAsync()
 
-      client.quit()
-    })
-})
+  await client.quitAsync()
+})()
