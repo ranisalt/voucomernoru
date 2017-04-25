d5389390af2689424aa65cd183f9b948a8bda152
Ranieri Althoff <ranisalt@gmail.com>
2017-04-25 10:45:56 -0300

    Drop unmaintained cca code

--- update.js
+++ update.js
@@ -1,14 +1,9 @@
 'use strict'
 
-const bluebird = require('bluebird')
 const cheerio = require('cheerio')
 const cloudinary = require('cloudinary')
-const pdf2table = require('pdf2table')
 const request = require('request-promise')
 
-// const ccaIgnores = new Set([
-//   'saladas', 'acompanhamentos quentes', 'carnes', 'sobremesa'
-// ])
 const replMap = {
   bife: 'chinelo',
   quibe: 'quibe :('
@@ -17,57 +12,8 @@ const replMap = {
 const regex = new RegExp(Object.keys(replMap).join('|'))
 const ruify = words => words.replace(regex, match => replMap[match])
 const sanitize = words => words.toLowerCase().replace('/', ' e ').trim()
-bluebird.promisifyAll(pdf2table)
 
-// const cca = async () => {
-//   const weekday = (new Date().getDay()) % 7
-
-//   // you'll get nothing from me on weekends!
-//   if (weekday === 0 || weekday === 6) {
-//     return
-//   }
-
-//   const $ = await request({
-//     gzip: true,
-//     transform: cheerio.load,
-//     uri: 'http://ru.ufsc.br/cca-2/'
-//   })
-
-//   const pdfUri = $('#content ul:first-of-type a:first-child').attr('href')
-//   const pdfBuffer = await request({
-//     encoding: null,
-//     uri: encodeURI(pdfUri)
-//   })
-
-//   const pdf = await pdf2table.parseAsync(pdfBuffer)
-//   const multi = client.multi().del('cca')
-//   for (const row of pdf) {
-//     const stuff = []
-//     row.forEach(currentValue => {
-//       if (/^[A-Z]/.test(currentValue.trim())) {
-//         stuff.push(currentValue.trim())
-//       } else {
-//         stuff.push(`${stuff.pop()} ${currentValue.trim()}`)
-//       }
-//     })
-
-//     if (stuff.length % 5 !== 0) {
-//       continue
-//     }
-
-//     stuff.map(element => element.toLowerCase())
-//       .filter((element, index) => {
-//         return (index % 5) === (weekday - 1) && !ccaIgnores.has(element)
-//       })
-//       .forEach(element => {
-//         multi.rpush('cca', element)
-//       })
-//   }
-
-//   await multi.execAsync()
-// }
-
-exports.ru = async function ru(db, date) {
+exports.ru = async function ru (db, date) {
   const $ = await request({
     gzip: true,
     transform: cheerio.load,
